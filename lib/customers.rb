efd28436b0e4fb13ece68e25298416c4eae07a96
Anna Marie Cooper and Masen Matthews <anna@>
2018-02-20 13:31:07 -0800

    save new customer spec passes.

--- lib/customers.rb
+++ lib/customers.rb
@@ -12,7 +12,32 @@ class Customers
   end
 
   def ==(another_customer)
-
+    self.customer_name().==(another_customer.customer_name()).&(self.id().==(another_customer.id()))
   end
 
-end
+  def customer_name
+    @customer_name
+  end
+
+  def id
+    @id
+  end
+
+  def self.all()
+    returned_customers = DB.exec("SELECT * FROM customers;")
+    customers = []
+    returned_customers.each() do |item|
+      customer_name = item.fetch("customer_name")
+      phone = item.fetch("phone")
+      animal_type_preference = item.fetch("animal_type_preference")
+      breed_preference = item.fetch("breed_preference")
+      id = item.fetch("id").to_i()
+      customers.push(Customers.new({:customer_name => customer_name, :phone => phone, :animal_type_preference => animal_type_preference, :breed_preference => breed_preference, :id => id}))
+    end
+    customers
+  end #all
+
+  def save
+    DB.exec("INSERT INTO customers(customer_name, phone, animal_type_preference, breed_preference, id) VALUES ('#{@customer_name}', '#{@phone}', '#{@animal_type_preference}', '#{@breed_preference}', #{@id});")
+  end #save
+end #Customer
--- spec/customers_spec.rb
+++ spec/customers_spec.rb
@@ -12,4 +12,18 @@ describe(Customers) do
     end
   end # ==
 
+  describe('.all') do
+    it('empty at first, but will populate later.') do
+      expect(Customers.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('adds a customer to an array of customers') do
+      first_customer = Customers.new({:customer_name => 'Susan', :phone => '503-333-3434', :animal_type_preference => 'dog', :breed_preference => 'Rottweiler', :id=> 1})
+      first_customer.save
+      expect(Customers.all()).to(eq([first_customer]))
+    end
+  end # save
+
 end #Customers
