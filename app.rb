8e467c385a86f4de3d34d2e02d6efc25766b6335
Anna-Marie Cooper <annamarie@rogueminx.com>
2018-02-22 11:16:41 -0800

    create functioning links between pages.

--- app.rb
+++ app.rb
@@ -16,6 +16,38 @@ get('/customers') do
   erb(:customer)
 end
 
+get('/customers/athroughz') do
+  erb(:customer/athroughz)
+end
+
+get('/customers/byanimaltype') do
+  erb(:byanimaltype)
+end
+
+get('/customers/animalsbybreed') do
+  erb(:animalsbybreed)
+end
+
 get('/employees') do
   erb(:employee)
+end
+
+get('/employees/addanimal') do
+  erb(:addanimal)
+end
+
+get('/employees/addcustomer') do
+  erb(:addcustomer)
+end
+
+get('/employees/allanimals') do
+  erb(:allanimals)
+end
+
+get('/employees/customersbybreed') do
+  erb(:customersbybreed)
+end
+
+get('/employees/adoption') do
+  erb(:adoption)
 end
