63a89e41ce070d5313ef91dd6bf360595e124d84
Anna-Marie Cooper <annamarie@rogueminx.com>
2018-02-22 08:28:47 -0800

    find an animal based on an ID.

--- lib/animal.rb
+++ lib/animal.rb
@@ -23,8 +23,18 @@ class Animal
   #We can insert a record into the database and have the ID of that new entry be returned to us by adding RETURNING id to the end of our INSERT command.
   #The pg gem always returns information in an array (technically it's not an array but it behaves more or less like one). When we save a list and want to get its ID, we have to use the first() method to take it out of the array. Then we can use the fetch method to select the ID.
 
-  def self.find
-
+  def self.find(id)
+    returned_animals = DB.exec("SELECT * FROM animal WHERE id = #{id};")
+    found_animal = []
+    returned_animals.each() do |item|
+      animal_name = item.fetch("animal_name")
+      gender = item.fetch("gender")
+      animal_breed = item.fetch("animal_breed")
+      date = item.fetch("date")
+      animal_type = item.fetch("animal_type")
+      found_animal.push(Animal.new({:animal_name => animal_name, :gender => gender, :animal_breed => animal_breed, :date => date, :animal_type => animal_type, :id => id}))
+    end
+  found_animal
   end
 
   def self.all()
